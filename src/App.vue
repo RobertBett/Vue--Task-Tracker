9472ef8fdb2f9dca6696b90c9ffebde75ffb7e34
RobertBett <robertbett6@icloud.com>
2021-11-01 01:26:04 -0400

    adds task tracker

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,133 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Header 
+        @toggle-task="toggleTask"
+        :showAddTask="showAddTask" 
+        title="Task Tracker"/>
+    <AddTask 
+        v-if="showAddTask" 
+        @add-task="createTask" 
+        title="Task Tracker"/>
+    <Tasks 
+        @delete-task="deleteTask"
+        @toggle-reminder="toggleReminder" 
+        :tasks="tasks"/>  
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Header from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
+import AddTask from './components/AddTask.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Header,
+    Tasks,
+    AddTask,
+  },
+  data() {
+    return {
+      tasks:[],
+      showAddTask:false
+    }
+  },
+  methods: {
+     async deleteTask(id){
+      if(confirm('Are you sure you want to delete')){
+        const response = await fetch(`api/tasks/${id+1}`, { method: 'DELETE' });
+        await response.json();
+        if(!response.ok) {
+          alert('Error deleting task')
+        }else{
+          this.tasks = this.tasks.filter(task => task.id !== id);
+        }
+      }
+    },
+    toggleReminder(id){
+      this.tasks = this.tasks.map((task) => {
+        console.log(id);
+        if(task.id === id){
+          return  { ...task, reminder: !task.reminder }
+        }
+        return task;
+      })
+    },  
+    async fetchTasks(){
+      const response = await fetch('api/tasks');
+      const data = await response.json();
+      return data;
+    },
+    async fetchTask(id){
+      const response = await fetch(`api/tasks/${id}`);
+      const data = await response.json();
+      return data;
+    },
+    async createTask(newTask) {
+      const response = await fetch('api/tasks', {
+          method: 'POST',
+          headers: { 
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(newTask),
+      })
+      const data = await response.json();
+      this.tasks = [...this.tasks, data];
+    },
+    toggleTask(){
+      this.showAddTask = !this.showAddTask;
+    }
+
+  },
+  async created() {
+    this.tasks =  await this.fetchTasks();
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid rgb(3, 6, 8);
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
+}
+
 </style>
---
+++ src/components/Task.vue
@@ -0,0 +1,43 @@
+<template>
+    <div  @dblclick="$emit('toggle-reminder', id)"  
+          :class="[reminder && 'reminder', 'task' ]"
+    >
+        <h3>
+            {{text}}
+             <i @click="$emit('delete-task', id)" class="fas fa-times"></i>    
+            </h3>
+        <p>{{day}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Task',
+        props:{
+            id:[String, Number],
+            text: String,
+            day: String,
+            reminder: Boolean,
+        },
+    }
+</script>
+
+<style scope>
+.fas {
+  color: red;
+}
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task.reminder {
+  border-left: 5px solid green;
+}
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
